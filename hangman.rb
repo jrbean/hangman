dcb31713b1d1b1ad91a0c91c66f383b4c4079b53
Justin Bean <beanjustinr@gmail.com>
2015-09-22 22:41:54 -0400

    Win possible, loss possible. Board building begun.

--- hangman.rb
+++ hangman.rb
@@ -8,30 +8,43 @@ class Hangman_game
     @answer = ("dog").chars
     @guessed_letters = []
     @wrong_letters = []
+    @board = []
   end
 
-  def game_over?
-    @guesses_left == 0 #|| won?
+  def draw_board
+    counter = 0
+    @answer.each do |l| if @guessed_letters.include? l
+        @board[counter] = letter.to_s + " "
+        counter += 1
+      else
+        @board[counter] = "_ "
+        counter += 1
+      end
+    end
   end
 
   def check_guess guessed_answer
     if @answer.include? (guessed_answer)
       puts "That's one."
-      @guessed_correct_letter = true
       @guessed_letters.push (guessed_answer)
-      puts @guessed_letters
+      puts @guessed_letters #need to figure out how to draw a board
     else
       puts "They're hanging amputees."
-      @guessed_correct_letter = false
       @wrong_letters.push (guessed_answer)
       @guesses_left -= 1
       puts "You've used: #{@wrong_letters}."
     end
   end
 
-  # def won?
-  #     @guessed_correct_answer
-  # end
+  def won?
+    @answer.all? do |g|
+      @guessed_letters.include?(g)
+    end
+  end
+
+  def game_over?
+    @guesses_left == 0 || won?
+  end
 
 end
 
@@ -40,6 +53,7 @@ end
 g = Hangman_game.new
 puts "Guess a letter to complete the word before he dies."
 puts "Your Word"
+puts "_ _ _"
 #g.draw_board
 
 until g.game_over?
